1b1cdcd1eeb199198f6058550a1f67211b2fb2cc
tim <ctom3560@gmail.com>
2025-09-06 00:06:43 +0800

    fix tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -11,17 +11,21 @@ from src.ca import Certificate, CertAuthority
 
 # Dummy message class for testing
 class DummyMsg:
-    def __init__(self, sender, sessionId, startTime, record, signatures):
+    def __init__(self, sender, sessionId, record, signatures):
         self.sender = sender
         self.sessionId = sessionId
-        self.startTime = startTime
         self.record = record
         self.signatures = signatures
 
     def digest(self):
-        b = f"{self.sender}{self.sessionId}{self.startTime}\
-                {self.record}".encode()
+        b = f"{self.sender}{self.sessionId}{self.record}".encode()
         return hashlib.sha256(b).digest()
+
+class DummyClock:
+    def __init__(self, round):
+        self._round = round
+    def now(self):
+        return self._round
 
 class TestValidator(unittest.TestCase):
     def setUp(self):
@@ -33,6 +37,7 @@ class TestValidator(unittest.TestCase):
         pubKey, priKey = genKeyPair()
         self.ca = CertAuthority(pubKey, priKey, self.sigMan)
         self.node_id = 2
+        self.clock = DummyClock(round=1)
 
         # Each peer gets their own keypair and certificate
         self.peer_keys = {}
@@ -49,12 +54,12 @@ class TestValidator(unittest.TestCase):
         sessionId = "sid"
         startTime = datetime.now()
         record = "+:42"
-        msg = DummyMsg(sender, sessionId, startTime, record, [])
+        msg = DummyMsg(sender, sessionId, record, [])
         # Simulate valid signature chain
         sig = self.sigMan.sign(msg, self.peer_keys[sender])
         msg.signatures = [[self.peer_certs[sender], sig]]
-        validator = Validator(self.recordPattern, self.ca, self.sigMan)
-        result = validator.validate(msg, self.roundTW, self.node_id, self.peers)
+        validator = Validator(self.recordPattern, self.clock, self.ca, self.sigMan)
+        result = validator.validate(msg, self.node_id, self.peers)
         self.assertTrue(result)
 
     def test_invalid_record_pattern(self):
@@ -62,11 +67,11 @@ class TestValidator(unittest.TestCase):
         sessionId = "sid"
         startTime = datetime.now()
         record = "bad_format"
-        msg = DummyMsg(sender, sessionId, startTime, record, [])
+        msg = DummyMsg(sender, sessionId, record, [])
         sig = self.sigMan.sign(msg, self.peer_keys[sender])
         msg.signatures = [[self.peer_certs[sender], sig]]
-        validator = Validator(self.recordPattern, self.ca, self.sigMan)
-        result = validator.validate(msg, self.roundTW, self.node_id, self.peers)
+        validator = Validator(self.recordPattern, self.clock, self.ca, self.sigMan)
+        result = validator.validate(msg, self.node_id, self.peers)
         self.assertFalse(result)
 
     def test_wrong_signer(self):
@@ -74,15 +79,15 @@ class TestValidator(unittest.TestCase):
         sessionId = "sid"
         startTime = datetime.now()
         record = "+:42"
-        msg = DummyMsg(sender, sessionId, startTime, record, [])
+        msg = DummyMsg(sender, sessionId, record, [])
         # Signature from a non-peer
         wrong_pk, wrong_sk = genKeyPair()
         wrong_cert = Certificate(99, wrong_pk)
         self.ca.sign(wrong_cert)
         sig = self.sigMan.sign(msg, wrong_sk)
         msg.signatures = [[wrong_cert, sig]]
-        validator = Validator(self.recordPattern, self.ca, self.sigMan)
-        result = validator.validate(msg, self.roundTW, self.node_id, self.peers)
+        validator = Validator(self.recordPattern, self.clock, self.ca, self.sigMan)
+        result = validator.validate(msg, self.node_id, self.peers)
         self.assertFalse(result)
 
     def test_duplicate_signer(self):
@@ -90,7 +95,7 @@ class TestValidator(unittest.TestCase):
         sessionId = "sid"
         startTime = datetime.now()
         record = "+:42"
-        msg = DummyMsg(sender, sessionId, startTime, record, [])
+        msg = DummyMsg(sender, sessionId, record, [])
         sig1 = self.sigMan.sign(msg, self.peer_keys[sender])
         sig2 = self.sigMan.sign(msg, self.peer_keys[sender])
         # Duplicate signer
@@ -98,8 +103,8 @@ class TestValidator(unittest.TestCase):
             [self.peer_certs[sender], sig1],
             [self.peer_certs[sender], sig2]
         ]
-        validator = Validator(self.recordPattern, self.ca, self.sigMan)
-        result = validator.validate(msg, self.roundTW, self.node_id, self.peers)
+        validator = Validator(self.recordPattern, self.clock, self.ca, self.sigMan)
+        result = validator.validate(msg, self.node_id, self.peers)
         self.assertFalse(result)
 
 if __name__ == '__main__':
